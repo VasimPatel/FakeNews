d2c371a4d048d35f4dd5ab6cbe4030c5350b06c1
DivineEnder <qwertydraw@gmail.com>
2017-03-20 19:54:58 -0400

    Updated the setup_db.py file to fix some small issues with the original database
    Fixed error on article_authors table where article_id was incorrectly spelled.
    
    Removed primary keys for linking tables as they required unique ids.
    
    Added indexes and many different columns accross all tables.

--- setup_db.py
+++ setup_db.py
@@ -1,7 +1,7 @@
 # @Author: DivineEnder <DivineHP>
 # @Date:   2017-03-04 23:27:36
-# @Last modified by:   DivineHP
-# @Last modified time: 2017-03-10 17:36:24
+# @Last modified by:   DivineEnder
+# @Last modified time: 2017-03-20 19:43:54
 
 import Utils.settings as settings
 settings.init()
@@ -9,78 +9,70 @@ settings.init()
 # Import required modules
 import os
 import Utils.db_utils as db_utils
+import Utils.connection_utils as glc
 
-@glc_database_command
-def setup_sources(glc):
-	return ("""CREATE TABLE sources (
+def setup_sources():
+	glc.execute_db_command("""CREATE TABLE sources (
 		source_id serial UNIQUE PRIMARY KEY,
 		name varchar(255) UNIQUE NOT NULL
-	)""", None)
+	)""")
 
-@glc_database_command
-def setup_articles(glc):
-	return ("""CREATE TABLE articles (
+def setup_articles():
+	glc.execute_db_command("""CREATE TABLE articles (
 		article_id serial UNIQUE PRIMARY KEY,
 		title varchar(255) NOT NULL,
 		url varchar(255) NOT NULL,
 		created_at TIMESTAMP WITH TIME ZONE NOT NULL,
 		content text NOT NULL,
 		source_id integer NOT NULL REFERENCES sources on DELETE RESTRICT
-	)""", None)
+	)""")
 
-@glc_database_command
-def setup_authors(glc):
-	return ("""CREATE TABLE authors (
+def setup_authors():
+	glc.execute_db_command("""CREATE TABLE authors (
 		author_id serial UNIQUE PRIMARY KEY,
 		first_name varchar(50) NOT NULL,
 		last_name varchar(50) NOT NULL
-	)""", None)
+	)""")
 
 def setup_linking_tables():
-	@glc_database_command
-	def setup_article_authors_link():
-		return ("""CREATE TABLE article_authors (
+	glc.execute_db_command("""CREATE TABLE article_authors (
 			article_id integer NOT NULL REFERENCES articles ON DELETE CASCADE,
 			author_id integer NOT NULL REFERENCES authors ON DELETE CASCADE
 		)""", None)
 
-	@glc_database_command
-	def setup_source_authors_link():
-		return ("""CREATE TABLE source_authors (
+	glc.execute_db_command("""CREATE TABLE source_authors (
 			source_id integer NOT NULL REFERENCES sources ON DELETE CASCADE,
 			author_id integer NOT NULL REFERENCES authors ON DELETE CASCADE
 		)""", None)
 
-	@glc_database_command
-	def setup_article_tags_link():
-		return ("""CREATE TABLE article_tags (
+	glc.execute_db_command("""CREATE TABLE article_tags (
 			article_id integer NOT NULL REFERENCES articles ON DELETE CASCADE,
 			tag_id integer NOT NULL REFERENCES tags ON DELETE CASCADE
 		)""")
 
-@glc_database_command
 def setup_tags():
-	return ("""CREATE TABLE tags (
+	glc.execute_db_command("""CREATE TABLE tags (
 		tag_id serial UNIQUE PRIMARY KEY,
 		tag_name varchar(255) UNIQUE
-	)""", None)
-
+	)""")
 
 def setup_indexes():
-	@glc_database_command
-	def setup_tag_name_index():
-		return ("""CREATE INDEX tag_name_skey ON tags (tag_name)""", None)
+	glc.execute_db_command("""CREATE INDEX tag_name_skey ON tags (tag_name)""")
+	glc.execute_db_command("""CREATE INDEX source_name_skey ON sources (name)""")
+	glc.execute_db_command("""CREATE INDEX author_fname_skey ON authors (first_name)""")
+	glc.execute_db_command("""CREATE INDEX author_lname_skey ON authors (last_name)""")
+	glc.execute_db_command("""CREATE INDEX author_fullname_skey ON authors (first_name, last_name)""")
+	glc.execute_db_command("""CREATE INDEX article_title_skey ON articles (title)""")
+	glc.execute_db_command("""CREATE INDEX author_source_skey ON articles (source_id)""")
+	glc.execute_db_command("""CREATE INDEX ar_au_article_skey ON article_authors (article_id)""")
+	glc.execute_db_command("""CREATE INDEX ar_au_author_skey ON article_authors (author_id)""")
+	glc.execute_db_command("""CREATE INDEX s_au_source_skey ON source_authors (source_id)""")
+	glc.execute_db_command("""CREATE INDEX s_au_author_skey ON source_authors (author_id)""")
+	glc.execute_db_command("""CREATE INDEX ar_t_article_skey ON article_tags (article_id)""")
+	glc.execute_db_command("""CREATE INDEX ar_t_tag_skey ON article_tags (tag_id)""")
 
-	@glc_database_command
-	def setup_source_name_index():
-		return ("""CREATE INDEX source_name_skey ON sources (name)""", None)
-
-	@glc_database_command
-	def setup_tag_name_index():
-		return ("""CREATE INDEX tag_name_skey ON tags (tag_name)""", None)
-
-@db_utils.new_connection(host = os.environ.get("DBHOST"), dbname = os.environ.get("DBNAME"), user = os.environ.get("DBUSER"), password = os.environ.get("DBPASS"), global_conn = True)
-def main(glc):
+@glc.new_connection(primary = True, pass_to_function = False)
+def main():
 	setup_sources()
 	setup_articles()
 	setup_authors()
