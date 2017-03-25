13b7d372fbc02a36cf5e30c2266b81a289165f4a
pferner710 <pferner@optonline.ent>
2017-03-25 18:30:03 -0400

    Make script to query November politico data and make associated word cloud

---
+++ test.py
@@ -0,0 +1,38 @@
+import Utils.settings as settings
+settings.init()
+
+from os import path
+from wordcloud import WordCloud
+
+import Utils.connection_utils as glc
+
+
+@glc.new_connection(primary = True, pass_to_function = False)
+def concatContent():
+	articles = glc.execute_db_query("SELECT content FROM articles WHERE created_at BETWEEN '2016-11-01 00:00:00' AND '2016-11-30 23:59:59'")
+	text = ""
+	for article in articles :
+		contents = article["content"]
+		text = text + contents
+	return text
+
+text = concatContent()
+# Generate a word cloud image
+wordcloud = WordCloud().generate(text)
+
+# Display the generated image:
+# the matplotlib way:
+import matplotlib.pyplot as plt
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+
+# lower max_font_size
+wordcloud = WordCloud(max_font_size=40).generate(text)
+plt.figure()
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+# The pil way (if you don't have matplotlib)
+# image = wordcloud.to_image()
+# image.show()
