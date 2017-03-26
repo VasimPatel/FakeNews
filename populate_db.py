83053eed5036bbcadbc7e08b7331587f83d4c097
DivineEnder <qwertydraw@gmail.com>
2017-03-26 01:32:22 -0400

    Fixed Brietbart reading. Populate should be ready

--- populate_db.py
+++ populate_db.py
@@ -2,7 +2,7 @@
 # @Date:   2017-03-08 13:49:12
 # @Email:  danuta@u.rochester.edu
 # @Last modified by:   DivineEnder
-# @Last modified time: 2017-03-22 18:23:42
+# @Last modified time: 2017-03-26 01:30:50
 
 import Utils.settings as settings
 settings.init()
@@ -13,23 +13,70 @@ import Utils.connection_utils as glc
 import Utils.edit_fakenews_db as edit
 import Utils.get_fakenews_db as db
 
+import sys
 import json
 import datetime
 import dateutil.parser as parser
+from unidecode import unidecode
 
 # Read the data from the json file
 def read_json_data(filename):
+	articles = []
+	# Map string currently building
+	article_dict_str = ""
+	# Whether or not currently reading a map (to parse out list syntax)
+	reading_map = False
+	# Make sure map does not end in something that is a string
+	ignore_map = False
+
 	with open(filename, "r") as file:
-		data = json.load(file)
-	return data
+		byte_data = file.read(9999999)
+		while(byte_data):
+			for i in range(0, len(byte_data)):
+				char = byte_data[i]
+				# Check for strings and don't check for map endings or startings within them
+				if char == "\"" and (not byte_data[i - 1] == "\\" or byte_data[i - 2] == "\\"):
+					ignore_map = not ignore_map
+					article_dict_str = article_dict_str + char
+				# Check for map starting
+				elif char == "{" and not ignore_map:
+					reading_map = True
+					article_dict_str = "{"
+				# Check for map ending
+				elif char == "}" and reading_map and not ignore_map:
+					reading_map = False
+					article_dict_str = article_dict_str + "}"
+
+					try:
+						article = json.loads(article_dict_str)
+						articles.append(article)
+					except Exception as e:
+						print("Problem with read. Map read was: ")
+						print(unidecode(article_dict_str))
+						print("\n")
+						input()
+
+						print("ERROR")
+						print(e)
+						input()
+				# Otherwise if reading map just add the next character
+				elif reading_map:
+					article_dict_str = article_dict_str + char
+
+			# Read next 9999999 bytes of data from file
+			byte_data = file.read(9999999)
+
+	print("Finished loading maps")
+	# Return list of parsed articles
+	return articles
 
 # Add the article to the database
-def add_json_article_to_db(article, source_name):
+def add_json_article_to_db(article, source_name, VERBOSE = False):
 	tag_ids = []
 	for tag in article["tags"]:
 		db_tag = db.get_tag_named(tag)
 		if not db_tag:
-			tag_ids.append(edit.add_tag(tag))
+			tag_ids.append(edit.add_tag(tag, VERBOSE = VERBOSE))
 		else:
 			tag_ids.append(db_tag['tag_id'])
 	tag_ids = list(set(tag_ids))
@@ -40,18 +87,18 @@ def add_json_article_to_db(article, source_name):
 		if len(names) == 2:
 			db_author = db.get_author_named(names[0], names[1])
 			if not db_author:
-				author_ids.append(edit.add_author(names[0], names[1]))
+				author_ids.append(edit.add_author(names[0], names[1], VERBOSE = VERBOSE))
 			else:
 				author_ids.append(db_author['author_id'])
 
-	# TODO: Fix datetime stripping so that time zones are accounted for
 	edit.add_article(article["title"],
 		article["url"],
 		parser.parse(article["date"]),
 		article["content"],
 		db.get_source_named(source_name)['source_id'],
 		author_ids,
-		tag_ids
+		tag_ids,
+		VERBOSE = VERBOSE
 	)
 
 # Add all the source data to the database (tracks and prints progress to console)
@@ -81,7 +128,7 @@ def add_source_data_to_db(source_data, source_name):
 @glc.new_connection(primary = True, pass_to_function = False)
 def main():
 	add_source_data_to_db(read_json_data("data/politico_data.json"), "Politico")
-	# add_source_data_to_db(read_json_data("data/bb_data.json"), "BreitBart")
+	add_source_data_to_db(read_json_data("data/bb_data.json"), "BreitBart")
 
 if __name__ == "__main__":
 	main()
