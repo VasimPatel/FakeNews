4d4b855cd9647e320a7338873b52febc769a4894
DivineEnder <qwertydraw@gmail.com>
2017-03-28 21:33:40 -0400

    Cleaning up extra files and creating a folder for writeup materials in data

--- test_connection.py
+++ test_connection.py
@@ -2,7 +2,7 @@
 # @Date:   2017-03-08 14:07:12
 # @Email:  danuta@u.rochester.edu
 # @Last modified by:   DivineEnder
-# @Last modified time: 2017-03-20 20:09:49
+# @Last modified time: 2017-03-28 21:31:20
 
 import Utils.settings as settings
 settings.init()
@@ -10,13 +10,6 @@ settings.init()
 import os
 import Utils.db_utils as db_utils
 import Utils.get_fakenews_db as db
-
-# def self_reference(func):
-# 	import functools
-# 	@functools.wraps(func)
-# 	def wrapper(*args, **kwargs):
-# 		return func(func, *args, **kwargs)
-# 	return wrapper
 
 @db_utils.new_connection(primary = True, pass_to_function = False)
 def main():
