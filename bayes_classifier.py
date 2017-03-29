62c05cd5f59c4655f90a4b5e7d602ba5359901ef
Eric Samuel Weiss <ericsweiss1@gmail.com>
2017-03-29 19:06:43 -0400

    Create Bayes classifier for requested sources

--- bayes_classifier.py
+++ bayes_classifier.py
@@ -9,10 +9,18 @@ settings.init()
 
 from nltk import word_tokenize
 import Utils.connection_utils as glc
+import Utils.bayes_utils as bayes
 
-glc.new_connection(primary = True, pass_to_function = False)
+@glc.new_connection(primary = True, pass_to_function = False)
 def main():
-	pol_articles = glc.execute_db_query("""SELECT content FROM articles WHERE source_id = 1 LIMIT 1000""")
+	articles = glc.execute_db_query("SELECT source_id, content FROM articles LIMIT 1000")
+	sources = glc.execute_db_query("SELECT source_id FROM sources")
+	# pol_articles = glc.execute_db_query("""SELECT content FROM articles WHERE source_id = 1 LIMIT 1000""")
+
+	class_dict = bayes.build_class_dict(articles, sources)
+
+	#print(class_dict[1].items())
+
 
 
 
