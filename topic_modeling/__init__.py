7d900824a5e9807eb09735800447e58531a6a657
Jona Scott <Jona Scott>
2017-04-10 19:44:46 -0400

    Cleaned up topic modeling

---
+++ topic_modeling/__init__.py
@@ -0,0 +1,1 @@
+__all__=['bow_model','word_cloud_models','LDA_model']
---
+++ topic_modeling/word_cloud_models.py
@@ -0,0 +1,22 @@
+import os
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+MODELS_DIR = "models/"
+
+final_topics = open(os.path.join(MODELS_DIR, "final_topics.txt"), 'r')
+curr_topic = 0
+for line in final_topics:
+    #line = line.strip()[line.rindex(":") + 2:]
+    scores = [float(x.split("*")[0]) for x in line.split(" + ")]
+    words = [x.split("*")[1] for x in line.split(" + ")]
+    freqs = {}
+    for word, score in zip(words, scores):
+        freqs[word] = score
+    wordcloud = WordCloud(width=120, height=120).generate_from_frequencies(freqs)
+    curr_topic += 1
+    # lower max_font_size
+    plt.figure()
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+final_topics.close()
